73410c6cb5f48e9f336d740a2d347233bd5ed31c
NataliBrz <ash_kronius@mail.ru>
2023-03-30 22:37:40 +0200

    +buttons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,26 @@ import React from "react";
 import '../components/cardStyle.css'
 import Cat from "./Cat";
 import Header from "./Header";
+import Button from "./Button";
+import ButtonAllWords from "./ButtonAllWords";
 
-
+const unresolvetWords = [
+  {
+    name: "cat",
+    description: "small animal",
+    translate: "Кот"
+  },
+  {
+    name: "dog",
+    description: "big animal",
+    translate: "Собака"
+  },
+  {
+    name: "bird",
+    description: "not animal",
+    translate: "Птица"
+  }
+]
 function Card({ word, description }) {
     return (
         
@@ -18,12 +36,16 @@ function Card({ word, description }) {
                     <input type="checkbox" id="cat" className="checkbox"/>
                     <label for="cat" className="check_agree">выключить котика</label>
                   </div>
-                  <div className="cardword">{word}</div>
+                  <div className="cardword">{unresolvetWords.name}</div>
                   <div className="carddescription">{description}</div>
                   <button className="cardButton">Проверить перевод</button>
+
                 </div>
                 </div>
-               
+                <div className="buttonsCard">
+               <Button/>
+               <ButtonAllWords/>
+               <Button/></div>
               </div>
             );
           
